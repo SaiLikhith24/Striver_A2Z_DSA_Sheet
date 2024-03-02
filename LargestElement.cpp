9f758bed7b40f313d5785e68b7a54d20958e8654
Sai Likhith <likhith.kdr@gmail.com>
2024-03-02 16:02:55 +0530

    Update LargestElement.cpp

--- LargestElement.cpp
+++ LargestElement.cpp
@@ -1,4 +1,10 @@
 int largestElement(vector<int> &arr, int n) {
+    
+    /*Using Sorting O(NlogN)
+        sort(arr.begin(),arr.end());
+        return arr[n-1];*/
+
+    //O(N)
     int maximum = INT_MIN;
     for(int i = 0 ; i < arr.size() ; i++)
         if(arr[i]>maximum)
