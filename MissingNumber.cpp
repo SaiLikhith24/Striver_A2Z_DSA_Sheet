639c2286580f357b1bb3114e6382786fb56d9485
Sai Likhith <likhith.kdr@gmail.com>
2024-03-02 17:24:25 +0530

    Create MissingNumber.cpp

---
+++ MissingNumber.cpp
@@ -0,0 +1,9 @@
+int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for (int i = 0; i <= n; i++)
+            sum += i;
+        for (int i = 0; i < n; i++)
+            sum -= nums[i];
+        return sum;
+}
