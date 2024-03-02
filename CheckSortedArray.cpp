da85a5779ceaf67404e3dba0cbb96342e75ebc89
Sai Likhith <likhith.kdr@gmail.com>
2024-03-02 16:29:21 +0530

    Update CheckSortedArray.cpp

--- CheckSortedArray.cpp
+++ CheckSortedArray.cpp
@@ -1,13 +1,13 @@
 int isSorted(int n, vector<int> a) {
     /* O(Nlog N)
-    vector<int> b=a;
+    vector<int> b = a;
     sort(b.begin(),b.end());
-    return (a==b);
+    return (a == b);
     */
 
     //O(N)
-    for(int i=0;i<a.size()-1;i++)
-        if(a[i]>a[i+1])
+    for (int i = 0; i < a.size()-1; i++)
+        if (a[i] > a[i+1])
             return false;
     return true;
 }
